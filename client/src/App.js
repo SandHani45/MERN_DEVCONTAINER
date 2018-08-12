96ec054f461063e3709ce14ee8ad5828fc9caec9
unknown <sandhani.sk45@gmail.com>
2018-08-13 00:29:36 +0530

    add documentation

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
